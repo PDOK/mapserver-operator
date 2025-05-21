a60716f9d8050e952d46e183c597353887f7c039
Jelle Dijkstra <jelle.dijkstra@kadaster.nl>
2025-05-21 12:49:21 +0200

    Removed cert-manager e2e tests

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -263,57 +263,57 @@ var _ = Describe("Manager", Ordered, func() {
 			))
 		})
 
-		It("should provisioned cert-manager", func() {
-			By("validating that cert-manager has the certificate Secret")
-			verifyCertManager := func(g Gomega) {
-				cmd := exec.Command("kubectl", "get", "secrets", "webhook-server-cert", "-n", namespace)
-				_, err := utils.Run(cmd)
-				g.Expect(err).NotTo(HaveOccurred())
-			}
-			Eventually(verifyCertManager).Should(Succeed())
-		})
+		//It("should provisioned cert-manager", func() {
+		//	By("validating that cert-manager has the certificate Secret")
+		//	verifyCertManager := func(g Gomega) {
+		//		cmd := exec.Command("kubectl", "get", "secrets", "webhook-server-cert", "-n", namespace)
+		//		_, err := utils.Run(cmd)
+		//		g.Expect(err).NotTo(HaveOccurred())
+		//	}
+		//	Eventually(verifyCertManager).Should(Succeed())
+		//})
+		//
+		//It("should have CA injection for WFS conversion webhook", func() {
+		//	By("checking CA injection for WFS conversion webhook")
+		//	verifyCAInjection := func(g Gomega) {
+		//		cmd := exec.Command("kubectl", "get",
+		//			"customresourcedefinitions.apiextensions.k8s.io",
+		//			"wfs..pdok.nl",
+		//			"-o", "go-template={{ .spec.conversion.webhook.clientConfig.caBundle }}")
+		//		vwhOutput, err := utils.Run(cmd)
+		//		g.Expect(err).NotTo(HaveOccurred())
+		//		g.Expect(len(vwhOutput)).To(BeNumerically(">", 10))
+		//	}
+		//	Eventually(verifyCAInjection).Should(Succeed())
+		//})
 
-		It("should have CA injection for WFS conversion webhook", func() {
-			By("checking CA injection for WFS conversion webhook")
-			verifyCAInjection := func(g Gomega) {
-				cmd := exec.Command("kubectl", "get",
-					"customresourcedefinitions.apiextensions.k8s.io",
-					"wfs..pdok.nl",
-					"-o", "go-template={{ .spec.conversion.webhook.clientConfig.caBundle }}")
-				vwhOutput, err := utils.Run(cmd)
-				g.Expect(err).NotTo(HaveOccurred())
-				g.Expect(len(vwhOutput)).To(BeNumerically(">", 10))
-			}
-			Eventually(verifyCAInjection).Should(Succeed())
-		})
-
-		It("should have CA injection for WMS conversion webhook", func() {
-			By("checking CA injection for WMS conversion webhook")
-			verifyCAInjection := func(g Gomega) {
-				cmd := exec.Command("kubectl", "get",
-					"customresourcedefinitions.apiextensions.k8s.io",
-					"wms..pdok.nl",
-					"-o", "go-template={{ .spec.conversion.webhook.clientConfig.caBundle }}")
-				vwhOutput, err := utils.Run(cmd)
-				g.Expect(err).NotTo(HaveOccurred())
-				g.Expect(len(vwhOutput)).To(BeNumerically(">", 10))
-			}
-			Eventually(verifyCAInjection).Should(Succeed())
-		})
-
-		It("should have CA injection for validating webhooks", func() {
-			By("checking CA injection for validating webhooks")
-			verifyCAInjection := func(g Gomega) {
-				cmd := exec.Command("kubectl", "get",
-					"validatingwebhookconfigurations.admissionregistration.k8s.io",
-					"mapserver-operator-validating-webhook-configuration",
-					"-o", "go-template={{ range .webhooks }}{{ .clientConfig.caBundle }}{{ end }}")
-				vwhOutput, err := utils.Run(cmd)
-				g.Expect(err).NotTo(HaveOccurred())
-				g.Expect(len(vwhOutput)).To(BeNumerically(">", 10))
-			}
-			Eventually(verifyCAInjection).Should(Succeed())
-		})
+		//It("should have CA injection for WMS conversion webhook", func() {
+		//	By("checking CA injection for WMS conversion webhook")
+		//	verifyCAInjection := func(g Gomega) {
+		//		cmd := exec.Command("kubectl", "get",
+		//			"customresourcedefinitions.apiextensions.k8s.io",
+		//			"wms..pdok.nl",
+		//			"-o", "go-template={{ .spec.conversion.webhook.clientConfig.caBundle }}")
+		//		vwhOutput, err := utils.Run(cmd)
+		//		g.Expect(err).NotTo(HaveOccurred())
+		//		g.Expect(len(vwhOutput)).To(BeNumerically(">", 10))
+		//	}
+		//	Eventually(verifyCAInjection).Should(Succeed())
+		//})
+		//
+		//It("should have CA injection for validating webhooks", func() {
+		//	By("checking CA injection for validating webhooks")
+		//	verifyCAInjection := func(g Gomega) {
+		//		cmd := exec.Command("kubectl", "get",
+		//			"validatingwebhookconfigurations.admissionregistration.k8s.io",
+		//			"mapserver-operator-validating-webhook-configuration",
+		//			"-o", "go-template={{ range .webhooks }}{{ .clientConfig.caBundle }}{{ end }}")
+		//		vwhOutput, err := utils.Run(cmd)
+		//		g.Expect(err).NotTo(HaveOccurred())
+		//		g.Expect(len(vwhOutput)).To(BeNumerically(">", 10))
+		//	}
+		//	Eventually(verifyCAInjection).Should(Succeed())
+		//})
 
 		// +kubebuilder:scaffold:e2e-webhooks-checks
 
